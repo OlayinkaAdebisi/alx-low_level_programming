d2f3712d41132678f3eda49f5bbd1c0d0f6954dd
Linux <adebisio024@gmail.com>
2022-09-08 20:51:01 -0700

    Finale

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,6 +5,6 @@
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19" , stdout);
 	return (1);
 }
