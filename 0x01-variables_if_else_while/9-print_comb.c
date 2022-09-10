25103eed3e0a56729b319488f3bb88e7712674ee
Linux <adebisio024@gmail.com>
2022-09-10 16:28:27 -0700

    advanced

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Descripition: 'single digit'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int digit = 0;
+
+while (digit <= 8)
+	{
+		putchar(digit + '0');
+		digit++;
+		putchar(',');
+		putchar(' ');
+	 }
+	putchar('9');
+	putchar('\n');
+	return (0);
+}
