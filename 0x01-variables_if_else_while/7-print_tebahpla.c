4efc3180200cdc86eb4c322704759100ddbd76ca
Linux <adebisio024@gmail.com>
2022-09-10 07:12:50 -0700

    reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char alpha = 'z';
+
+	while (alpha >= 'a')
+	{
+	putchar(alpha);
+	alpha--;
+	}
+	return (0);
+}
