1fec50c53c35ab6fb20dd63ef09c5f8615e4b989
Linux <adebisio024@gmail.com>
2022-09-10 04:15:48 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Descripition: 'Printing of numbers'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for(num = 0;  num <= 10; )
+	{
+		printf("%d", num);
+		num++;
+	}
+		putchar('\n');
+	return (0);
+}
