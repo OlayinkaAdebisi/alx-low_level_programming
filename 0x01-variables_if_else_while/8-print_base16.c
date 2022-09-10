ed08124be585704420db66e67f0af2fe77ff1a4d
Linux <adebisio024@gmail.com>
2022-09-10 07:47:09 -0700

    hexa

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int hex = 0;
+
+	while (hex <= 'f')
+	{
+		putchar(hex);
+		hex++;
+	}
+putchar('\n');
+	return (0);
+}
