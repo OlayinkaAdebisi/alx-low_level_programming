7eb26070812ab082ef187b14ee1511fcb051f8d7
Linux <adebisio024@gmail.com>
2022-09-10 04:57:34 -0700

    fix

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'Numberz'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	while((n = 0); n < 10; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	
+	return (0);
+}
