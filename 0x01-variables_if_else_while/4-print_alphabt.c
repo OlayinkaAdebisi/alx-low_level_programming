d887dc20dd734313bace967ede65577ecc235707
Linux <adebisio024@gmail.com>
2022-09-09 22:05:37 -0700

    If statement

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,7 +11,7 @@
 int main(void)
 {
 		char ch = 'a';
-		
+
 		while (ch <= 'z')
 		{
 			if (ch == 'e' || ch == 'q')
@@ -20,6 +20,5 @@ int main(void)
 			++ch;
 		}
 		putchar('\n');
-		
 		return (0);
 }
