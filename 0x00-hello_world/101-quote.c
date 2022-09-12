247dfa0af8e49a6e908f382c13c5ba6fdd0c8cce
Linux <adebisio024@gmail.com>
2022-09-12 09:09:47 -0700

    Finale

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry Point
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char dad[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	fwrite(1, dad, 59);
+	char dd[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1, dd, 59);
 
 	return (1);
 }
