2149d285a1cc9a45f8e16aa8db9dfb2b139e795e
Linux <adebisio024@gmail.com>
2022-09-14 16:55:12 -0700

    jjjjjjj

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main - Entry point
  * Return: Always 0 (Success
@@ -8,8 +8,8 @@ int main(void)
 	int ch;
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
-		putchar(ch);
+		_putchar(ch);
 	}
-		putchar('\n');
+		_putchar('\n');
 		return (0);
 }
