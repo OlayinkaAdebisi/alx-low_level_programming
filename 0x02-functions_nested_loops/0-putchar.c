6c1190d8c656dc0faadbc6bb8451b09c4d40b342
Linux <adebisio024@gmail.com>
2022-09-15 04:04:33 -0700

    Hope

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,18 @@
 #include "main.h"
-
 /**
  * main - Entry point
  * Return: Always 0 (Success)
  */
 int main(void)
-{
-	char str[] = "_putchar";
-	int ch;
 
-	for (ch = '0'; ch < '8'; ch++)
-			putchar(str[ch]);
-		putchar('\n');
-		
+{
+	char str[8] = "_putchar";
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,1 @@
-#ifndef HOLBERTON_H_INCLUDED
-#define HOLBERTON_H_INCLUDED
-
-#include <stdio.h>
-
 int _putchar(char c);
-void print_alphabet(void);
-
-#endif
