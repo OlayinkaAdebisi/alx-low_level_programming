66c3df70f7419955cafe3dd7f9d0eb19ff348955
Linux <adebisio024@gmail.com>
2022-09-15 02:29:48 -0700

    i need sub

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ll[] = "_putchar";
+	int ch;
+	for (ch = '0'; ch < '8'; ch++)
+	{
+		putchar(ch);
+		putchar('\n');
+	}
+	return (0);
+}
