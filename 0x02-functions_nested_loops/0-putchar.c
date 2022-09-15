803fba980a0fd43c05ff65882bd301654d32e0da
Linux <adebisio024@gmail.com>
2022-09-15 02:43:08 -0700

    hhh

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include"main.h"
+#include "main.h"
 
 /**
  * main - Entry point
@@ -6,12 +6,12 @@
  */
 int main(void)
 {
-	char ll[] = "_putchar";
+	char str[] = "_putchar";
 	int ch;
+
 	for (ch = '0'; ch < '8'; ch++)
-	{
-		putchar(ch);
+			putchar(str[ch]);
 		putchar('\n');
-	}
+		
 	return (0);
 }
