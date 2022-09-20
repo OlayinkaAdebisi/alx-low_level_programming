6904601d960493cda9c12ca39a212a00f2628c85
Linux <adebisio024@gmail.com>
2022-09-19 20:28:45 -0700

    life hard

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - numbers of string
+ * @s: 'pointer'
+ * Return: (_strlen)
+ */
+int _strlen(char *s)
+{
+	int dad;
+	for (dad = 0; *s != '\0'; s++)
+		dad++;	
+return (dad);
+}
