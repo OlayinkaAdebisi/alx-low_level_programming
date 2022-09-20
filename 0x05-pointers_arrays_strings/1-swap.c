1de0a99d4d5a25185edd03b415273423acee5dca
Linux <adebisio024@gmail.com>
2022-09-19 19:08:02 -0700

    :thunder: :computer: :boy:

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * swap_int - swap values of d two integers
+ * @a: 'first integer'
+ * @b: 'second integer'
+ * Return: (swap_int)
+ */
+void swap_int(int *a, int *b)
+{
+	*a = *b;
+	*b = 98;
+}
