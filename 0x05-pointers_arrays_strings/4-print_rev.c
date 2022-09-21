deed2cfb3af1bd342bc51e57a26ca40fb3a3239f
Linux <adebisio024@gmail.com>
2022-09-20 17:26:53 -0700

    :hard:

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_rev - printing string in reverse
- * @str: 'string to be printed in reverse'
+ * @s: 'string to be printed in reverse'
  * Return: (str)
  */
 void print_rev(char *s)
@@ -9,7 +9,7 @@ void print_rev(char *s)
 	int i;
 
 	for (i = 0; s[i] != '\0'; i++)
-		;	
+		;
 	for (--i; i >= 0; i--)
 		_putchar(s[i]);
 	_putchar('\n');
