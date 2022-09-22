4e4d756ece91731bb587b7ae16add013797d3df4
Linux <adebisio024@gmail.com>
2022-09-22 16:42:04 -0700

    :ship:

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,20 +1,19 @@
 #include "main.h"
 /**
- * strcat - string cat
+ * _strcat - string cat
  * @dest: string
  * @src: string2
  * Return: (_strccat)
  */
 char *_strcat(char *dest, char *src)
 {
-	int k,l;
-	
-	/* counting numbers of characters in dest */ 
+	int k, l;
+		/* counting numbers of characters in dest */
 	k = 0;
 	while (dest[k])
 		k++;
 	/* concatenate dest and src */
 	for (l = 0; src[l]; l++)
-		dest[k++] = src[l];	
-	return 0;
+		dest[k++] = src[l];
+	return (0);
 }
