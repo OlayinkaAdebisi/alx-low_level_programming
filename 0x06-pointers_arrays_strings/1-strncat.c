841e9539a5d1ebf485e9d3eb444d3de5c246f1e7
Linux <adebisio024@gmail.com>
2022-09-23 09:10:35 -0700

    :rock:

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,15 +4,17 @@
  * @dest: string to be concate...
  * @src: string to be concate...
  * @n: integer
+ * Return: (_strncat)
  */
 char *_strncat(char *dest, char *src, int n)
 {
 	int len, i;
+
 	for (len = 0; dest[len] != '\0'; len++)
 		;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
-			dest[len + i] = src[i];
+		dest[len + i] = src[i];
 
 	/*should end with a end null char*/
 	dest[len + i] = '\0';
