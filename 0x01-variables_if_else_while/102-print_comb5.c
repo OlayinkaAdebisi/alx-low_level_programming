4caf3564f505d59874c5e0bc39e26607bf04e708
Linux <adebisio024@gmail.com>
2022-09-24 10:19:07 -0700

    vomit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,40 +1,39 @@
 #include<stdio.h>
 
 /**
- *  * main - Entry Point
- *   * Description: Write a program that prints all possible
- *    * different combinations of two digits
- *     * Return: 0
- *      */
-
+ * main - Entry Point
+ * Description: Write a program that prints all possible
+ * different combinations of two digits
+ * Return: 0
+ */
 int main(void)
 {
-		int firstDigit = 0, seconDigit;
+	int firstDigit = 0, seconDigit;
 
-			while (firstDigit <= 99)
-					{
-								seconDigit = firstDigit;
-										while (seconDigit <= 99)
-													{
-																	if (seconDigit != firstDigit)
-																					{
-																										putchar((firstDigit / 10) + 48);
-																														putchar((firstDigit % 10) + 48);
-																																		putchar(' ');
-																																						putchar((seconDigit / 10) + 48);
-																																										putchar((seconDigit % 10) + 48);
+	while (firstDigit <= 99)
+	{
+		seconDigit = firstDigit;
+		while (seconDigit <= 99)
+		{
+			if (seconDigit != firstDigit)
+			{
+				putchar((firstDigit / 10) + 48);
+				putchar((firstDigit % 10) + 48);
+				putchar(' ');
+				putchar((seconDigit / 10) + 48);
+				putchar((seconDigit % 10) + 48);
+				if (firstDigit != 98 || seconDigit != 99)
+				{
+					putchar(',');
+					putchar(' ');
 
-																																														if (firstDigit != 98 || seconDigit != 99)
-																																																			{
-																																																									putchar(',');
-																																																														putchar(' ');
-																																																																		}
-																																																	}
-																				++seconDigit;
-																						}
-												++firstDigit;
-													}
-				putchar('\n');
+				}
+			}
+			++seconDigit;
+		}
+		++firstDigit;
+	}
+	putchar('\n');
 
-					return (0);
+	return (0);
 }
