6a9fceafa46bc0a1a5145fde97206c55f6d4a7f0
Linux <adebisio024@gmail.com>
2022-09-24 08:16:53 -0700

    life hard

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * cap_string - cap str
+ * @str: sring
+ * Return:(cap_string)
+ */
+char *cap_string(char *str)
+{
+	int i;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+		if (i == 0)
+		{
+			str[i] -= 32;
+		}
+		if (str[i - 1] == 32 || str[i - 1] == 9 || str[i - 1] == 10 ||
+				str[i - 1] == 44 || str[i - 1] == 59 || str[i - 1] == 46 ||
+				str[i - 1] == 33 || str[i - 1] == 63 || str[i - 1] == 34 ||
+				str[i - 1] == 40 || str[i - 1] == 41 || str[i - 1] == 123 ||
+				str[i - 1] == 124)
+		{
+			str[i] -= 32;
+		}
+		}
+		i++;
+	}
+	return (str);
+}
