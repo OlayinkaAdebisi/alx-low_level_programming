e8270f0f1780ad87a9b1d5cbd2f36bc5c3f2c586
Linux <adebisio024@gmail.com>
2022-09-25 18:27:54 -0700

    :moon: :light:

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
- *
- *
+ * _islower - return o and 1
+ * @c: character
+ * Return: (c)
  */
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
 	{
-		return(1);	
+	return (1);
 	}
 	return (0);
 }
