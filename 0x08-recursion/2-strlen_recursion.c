afa5a2b5638313895e7df0c245b773026c0fb9a8
Linux <adebisio024@gmail.com>
2022-09-28 10:35:13 -0700

    3rd is going

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,14 +1,15 @@
 #include "main.h"
 /**
- *
- *
- *
+ * _strlen_recursion - length of string
+ * @s: string
+ * Return: (_strlen_recursion)
  */
 int _strlen_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		_strlen_recursion(s + 1);
+		return (1 + _strlen_recursion(s + 1));
 	}
-	return (*s);
+	else
+	return (0);
 }
