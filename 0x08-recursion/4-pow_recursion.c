803968209ea52286c2080f0a6f89b1bd28f0b087
Linux <adebisio024@gmail.com>
2022-09-28 20:36:54 -0700

    is going

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -6,8 +6,14 @@
  */
 int _pow_recursion(int x, int y)
 {
-	if (y >= 0)
-		return (_pow_recursion(x, y));
-	else 
+	if (y < 0)
+	{
 		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (0);
+	}
+	else 
+		return (_pow_recursion(x, y));
 }
