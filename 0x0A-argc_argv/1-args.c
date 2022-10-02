72b47fa1949fb86786ae5f005ce1db2abec59621
Linux <adebisio024@gmail.com>
2022-10-01 19:51:11 -0700

    2nd is going

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - index
+ * @argc: counts
+ * @argv: array
+ * Return: (Success)
+ */
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	int i, n;
+
+	for (i = 0; i < argc; i++)
+	{
+		n = i;
+	}
+	printf("%d\n", n);
+	return (0);
+}
