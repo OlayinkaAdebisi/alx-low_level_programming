3ecfaaae52e7a9b4ff03de93d6d045c51e0b733b
Linux <adebisio024@gmail.com>
2022-10-01 20:02:23 -0700

    3rd is going

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - advances
+ * @argc: counts numbers of argu on cmd line
+ * @argv: array of argc
+ * Return: (main)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
