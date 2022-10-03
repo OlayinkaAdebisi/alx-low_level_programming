cbf6cac36ff1a6a3cda6ebeefa5941fa76f87d27
Linux <adebisio024@gmail.com>
2022-10-02 17:17:24 -0700

    4th is going

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,11 +8,20 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, n;
+	int i = 1, n;
 
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
 	for (i = 1; i < argc; i++)
 	{
 		n = atoi(argv[1]) * atoi(argv[2]);
 	}
 	printf("%d\n", n);
+	}
+	return (0);
 }
