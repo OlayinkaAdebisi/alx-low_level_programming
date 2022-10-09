24fef4e92335c09e8a37a52b02c7f553c9f7777b
Linux <adebisio024@gmail.com>
2022-10-09 07:08:12 -0700

    malloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -7,14 +7,14 @@
  */
 char *_strdup(char *str)
 {
-	unsigned int m = 0,n;
-	char *c;
+	unsigned int m = 0,n = 0;
+	char c = 0;
 
 	str = malloc(sizeof(char) * n);
 		
-	for (n = 0; n < m ; n++)
-		str[n] = c[n];
-	m++;
+	for (; n < m ; n++)
+		str[n] = c;
+	str++;
 	if (str == NULL)
 		return (NULL);
 	return (str);
