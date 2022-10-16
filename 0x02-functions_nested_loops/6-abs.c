2f018227ef327ec818b9fa197cb8d975d7677cd2
Linux <adebisio024@gmail.com>
2022-10-15 17:04:54 -0700

    hhj

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ */
+int _abs(int n)
+{
+	int i;
+
+	i = ((n * i ));
+	if (n >= 48 && n <= 57)
+		_putchar(n);
+	else 
+		_putchar(i);
+		return (i);	
+	return (n);
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *
+ *
+ */
+int print_last_digit(int i)
+{
+	int m;
+	m = i % 10;
+
+	_putchar(m);
+	return (m);
+}
