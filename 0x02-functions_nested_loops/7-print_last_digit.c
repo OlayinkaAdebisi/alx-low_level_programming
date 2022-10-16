f6696867d25f4aabcb9f73e0c59420ad93ae0f1c
Linux <adebisio024@gmail.com>
2022-10-15 17:18:57 -0700

    hhj

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,13 +1,19 @@
 #include "main.h"
 /**
- *
- *
+ * print_last_digit - last digit
+ * @i: number
+ * Return: (m)
  */
 int print_last_digit(int i)
 {
 	int m;
-	m = i % 10;
 
-	_putchar(m);
+	if (i < 0)
+	{
+		m = (-1 * (i % 10));
+	}
+	else
+		m = (i % 10);
+	_putchar(m + '0');
 	return (m);
 }
