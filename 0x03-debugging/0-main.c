7b2982e65be8e368e521f150f5d6a12d21fb5050
Linux <adebisio024@gmail.com>
2022-10-17 10:24:30 -0700

    betty

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include"main.h"
 
 /**
 * main - tests function that prints if integer is positive or negative
@@ -8,7 +8,9 @@
 int main(void)
 {
 	int i;
-		i = 0;
+
+	i = 98 % 2;
 	positive_or_negative(i);
+
 	return (0);
 }
