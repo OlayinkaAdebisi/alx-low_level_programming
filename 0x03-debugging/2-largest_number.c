a106705fb4cb86adfae04bfffa2eb814c0dc43bd
Linux <adebisio024@gmail.com>
2022-10-17 18:10:25 -0700

    ff

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,15 +12,15 @@ int largest_number(int a, int b, int c)
 {
     int largest;
 
-    if (a > b && a > c && a > 0)
+    if (a > b && a > c )
     {
         largest = a;
     }
-    else if (b > a && b > c && b > 0)
+    else if (b > a && b > c )
     {
         largest = b;
     }
-    else 
+    else if (c > a && c > b )
     {
         largest = c;
     }
