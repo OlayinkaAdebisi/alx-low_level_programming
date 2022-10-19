d90f10d8e41b762c7fbefa375f58111f8fe8d037
Linux <adebisio024@gmail.com>
2022-10-19 09:55:44 -0700

    finale

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,65 +1,22 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_to_98 - print
- * @n: number
- * Return: (n)
+ * print_to_98 - print all natural numbers from n to 98.
+ * @n: the number to start counting from n to 98
+ * Return: Always 0.
  */
 void print_to_98(int n)
 {
-	if (n == 0)
-	{
-		for (n = 0; n <= 98; n++)
-		{
-			printf("%d", n);
-			if (n < 98)
-			{
-				printf(",");
-				printf(" ");
-			}
-		}
-	}
-	else if (n == 98)
-	{
-		printf("\n98\n");
-	}
-	else if (n == 111)
-	{
-		for (n = 111; n >= 98; n--)
-		{
-			printf("%d", n);
-			if (n > 98)
-			{
-				printf(",");
-				printf(" ");
-			}
-		}
-		printf("\n");
-	}
-	else if (n == 81)
-	{
-		for (n = 81; n <= 98; n++)
-		{
-			printf("%d", n);
-			if (n < 98)
-			{
-				printf(",");
-				printf(" ");
-			}
-		}
-		printf("\n");
-	}
-	else if (n == -10)
-	{
-		for (n = -10; n <= 98; n++)
-		{
-			printf("%d", n);
-			if (n < 98)
-			{
-				printf(",");
-				printf(" ");
-			}
-		}
-		printf("\n");
-	}
+if (n < 98)
+{
+for (n = n; n < 98; n++)
+printf("%d, ", n);
+printf("%d\n", 98);
+}
+else
+{
+for (n = n; n > 98; n--)
+printf("%d, ", n);
+printf("%d\n", 98);
+}
 }
