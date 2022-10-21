ce10e9e435498987e910f90e8af3d5684333377b
Linux <adebisio024@gmail.com>
2022-10-21 10:28:27 -0700

    joy is coming

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * puts2 - print even
+ * @str: string to be printed
+ * Return: (0)
+ */
+void puts2(char *str)
+{
+	int i;
+
+	/*counts the number(in twos) of strings in str*/
+	for (i = 0; str[i] != '\0'; i = i + 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
