01bea0e0ec76d453dfe4353d8f1d81e441f46206
Linux <adebisio024@gmail.com>
2022-10-22 07:22:09 -0700

    8th nice comment

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string
+ * @str: string to be printed
+ * Return: (void)
+ */
+void puts_half(char *str)
+{
+	int i, n;
+
+	for (i = 0; str[i] != '\0'; i++)/*length of string*/
+		;
+	if (i % 2 == 0)/*for even numbers*/
+	{
+		for (n = (i / 2); str[n] != '\0'; n++)
+			_putchar(str[n]);
+	}
+	else /*for odd numbers*/
+		for (n = (i - 1) / 2; str[n] != '\0'; n++)
+			_putchar(str[n]);
+	_putchar('\n');
+}
